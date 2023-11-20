bccf75b53eb4e370b7ef01430d2c77e2785ad5b2
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-11-20 21:28:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 // Function to simulate signing in
 function signIn() {
-    // Get values from the form
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // In a real application, you'd perform server-side authentication here
-    // For simplicity, we'll just check if the username and password are not empty
     if (username.trim() !== '' && password.trim() !== '') {
-        // Save user information to local storage
         saveUserInfo(username);
-
-        // Hide the sign-in section
         document.getElementById('signin-section').style.display = 'none';
-
-        // Show the homepage section
         document.getElementById('homepage-section').style.display = 'block';
+        document.getElementById('username-display').innerText = username;
     } else {
         alert('Please enter a valid username and password.');
     }
@@ -25,18 +18,38 @@ function saveUserInfo(username) {
     localStorage.setItem('username', username);
 }
 
-// Function to check if the user is already signed in
-function checkSignIn() {
+// Function to simulate interacting with a virtual agent or a human
+function interact(interactionType) {
     const username = localStorage.getItem('username');
-
+    const resultElement = document.getElementById('interaction-result');
+    
     if (username) {
-        // User is signed in, show the homepage section
-        document.getElementById('signin-section').style.display = 'none';
-        document.getElementById('homepage-section').style.display = 'block';
+        let message = '';
+
+        if (interactionType === 'virtual') {
+            // Redirect to the virtual agent page
+            window.location.href = 'virtual-agent.html';
+            return;
+        } else if (interactionType === 'human') {
+            // Redirect to the human volunteers page
+            window.location.href = 'human-volunteers.html';
+            return;
+        }
+
+        resultElement.innerHTML = `<p>${message}</p>`;
+    } else {
+        document.getElementById('signin-section').style.display = 'block';
+        document.getElementById('homepage-section').style.display = 'none';
+        alert('Please sign in first.');
     }
 }
 
 // Check if the user is already signed in on page load
 window.onload = function () {
-    checkSignIn();
+    const username = localStorage.getItem('username');
+    if (username) {
+        document.getElementById('signin-section').style.display = 'none';
+        document.getElementById('homepage-section').style.display = 'block';
+        document.getElementById('username-display').innerText = username;
+    }
 };
